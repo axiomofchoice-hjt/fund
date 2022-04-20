d65a5e7540e634a405fc460504b297e61727ff12
Axiomofchoice <1939696303@qq.com>
2022-04-20 16:33:50 +0800

    .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <el-input placeholder="请输入内容" v-model="input" clearable> </el-input>
+    <el-radio-group v-model="radio1" style="float: right">
+      <el-radio-button label="A"></el-radio-button>
+      <el-radio-button label="B"></el-radio-button>
+      <el-radio-button label="C"></el-radio-button>
+    </el-radio-group>
+    <el-button type="primary" style="float: right">筛选</el-button>
   </div>
 </template>
 
