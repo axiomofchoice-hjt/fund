a2ada1168c216a4a2af45112ed900ce1f3ae53a5
Axiomofchoice <1939696303@qq.com>
2022-04-20 16:17:36 +0800

    .

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-input placeholder="请输入内容" v-model="input" clearable> </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+// vue-router
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+// element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+import App from './App.vue'
+import router from './router';
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
